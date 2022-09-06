3b51cb107383bbd4e86c16c06ff557b12d4fcc01
CoraSan1 <nguyendinh070199@gmail.com>
2022-09-06 10:36:41 +0700

    KT so nguyen to

--- src/ThucHanh/KiemTraSoNguyenTo.java
+++ src/ThucHanh/KiemTraSoNguyenTo.java
@@ -1,7 +1,32 @@
 package ThucHanh;
 
+import java.util.Scanner;
+
 public class KiemTraSoNguyenTo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so can kiem tra: ");
+        int number = sc.nextInt();
+        if (number < 2){
+            System.out.print("Day khong phai so nguyen to");
+        }
+        else{
+            int i = 2;
+            boolean check = true;
+            while (i < number){
+                if (number % i == 0){
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check){
+                System.out.print(number + " la so nguyen to");
+            }else {
+                System.out.print(number + " khong la so nguyen to");
+            }
+
+        }
 
     }
 }
